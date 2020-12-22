97c006250426c1c333581c617c75e0a53f770c96
Roddy <github@roddymacsween.co.uk>
2020-12-22 17:45:37 +0000

    Initial commit

---
+++ bin/main.ml
@@ -0,0 +1,18 @@
+open Opium
+
+let handler req =
+  Printf.sprintf "Hello, %s!\n" (Router.param req "name")
+  |> Response.of_plain_text |> Lwt.return
+
+let port =
+  Sys.getenv_opt "PORT" |> Option.value ~default:"3000" |> int_of_string
+
+let () =
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Logs.set_level (Some Logs.Info)
+
+let () =
+  App.empty |> App.port port
+  |> App.middleware Middleware.logger
+  |> App.get "/hello/:name" handler
+  |> App.run_command
